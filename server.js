97e45caab7a080cd9c2db5177201901d18d03ae4
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2026-08-11 22:58:28 +0900

    アップロードエラー解消

--- server.js
+++ server.js
@@ -161,6 +161,17 @@ const authenticateToken = asyncHandler(async (req, res, next) => {
 // -----------------------------
 // API エンドポイント
 // -----------------------------
+// Firebase クライアント設定を返す（公開して問題ない値のみ）
+app.get('/api/config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  });
+});
 
 // ログインチェック（BAN 判定）
 app.post(
